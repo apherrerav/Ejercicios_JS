503995b48f51608604d47952f709d4351401a9d9
apherrerav <adriana9215@hotmail.com>
2026-03-05 20:17:34 -0500

    feat: ejercicio 26

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -197,6 +197,7 @@ function esPositivo(numero) {
 
 // Test 26
 function agregarSimboloExclamacion(str) {
+	return str+"!"
 	// Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
 	// Ejemplo: "hello world" pasaría a ser "hello world!"
 	// Tu código:
