0670cbf7e860f402d143e751de6f4787d4919082
apherrerav <adriana9215@hotmail.com>
2026-03-05 20:09:25 -0500

    feat: ejercicio 22

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -157,6 +157,7 @@ function elevar(num, exponent) {
 
 // Test 22
 function redondearNumero(num) {
+	return Math.round (num);
 	// Redondea "num" al entero más próximo y devuélvelo
 	// Tu código:
 }
