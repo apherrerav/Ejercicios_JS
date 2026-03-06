4cbfcac0f39c746a0ed515d9a1f890197031005b
apherrerav <adriana9215@hotmail.com>
2026-03-05 20:29:06 -0500

    feat: ejercicio 32

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -242,6 +242,8 @@ function areaDelTriangulo(base, altura) {
 
 // Test 32
 function deEuroAdolar(euro) {
+	let dolar = 1.20*euro
+	return dolar;
 	//Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
 	//como parámetro un número de euros y calcule el cambio en dólares.
 	//Escribe tu código aquí
