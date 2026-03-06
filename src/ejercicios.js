8620ea9bb55dcebc5317cd4cafecab38cd8c5288
apherrerav <adriana9215@hotmail.com>
2026-03-05 20:37:23 -0500

    feat: ejercicio del 33 al 35

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -251,6 +251,16 @@ function deEuroAdolar(euro) {
 
 // Test 33
 function esVocal(letra) {
+	letra.length === 1
+	if(letra.length > 1){
+		return "Dato incorrecto";
+	}
+	else if(['a','e','i','o','u'].includes(letra)) {
+		return "Es vocal";
+	}
+	else if(letra.toLowerCase()) {
+		return "Dato incorrecto";
+	}
 	//Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
 	//Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle
 	//que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
